e8257e90ca35a200dfdcc373cf7967d366b4b052
root <wangtongpy@foxmail.com>
2024-04-20 12:34:57 +0800

    通用化配置logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,22 +2,101 @@ package logger
 
 import (
 	"github.com/natefinch/lumberjack"
-	"github.com/reggiepy/LogBeetle/pkg/config"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 var Logger *zap.Logger
 
+type Config struct {
+	LogFile    string `json:"LogFile" yaml:"LogFile"`       // 日志文件名
+	MaxSize    int    `json:"MaxSize" yaml:"MaxSize"`       // 日志文件大小限制，单位为 MB
+	MaxBackups int    `json:"MaxBackups" yaml:"MaxBackups"` // 最大保留的旧日志文件数量
+	MaxAge     int    `json:"MaxAge" yaml:"MaxAge"`         // 旧日志文件保留天数
+	Compress   bool   `json:"Compress" yaml:"Compress"`     // 是否压缩旧日志文件
+	LogLevel   string `json:"LogLevel" yaml:"LogLevel"`     // 日志等级
+	LogFormat  string `json:"LogFormat" yaml:"LogFormat"`   // 日志等级
+}
+
+type ConfigOption func(loggerConfig *Config) error
+
+// WithLogFile 设置日志文件名
+func WithLogFile(logFile string) ConfigOption {
+	return func(loggerConfig *Config) error {
+		loggerConfig.LogFile = logFile
+		return nil
+	}
+}
+
+// WithMaxSize 设置日志文件大小限制，单位为 MB
+func WithMaxSize(maxSize int) ConfigOption {
+	return func(loggerConfig *Config) error {
+		loggerConfig.MaxSize = maxSize
+		return nil
+	}
+}
+
+// WithMaxBackups 设置最大保留的旧日志文件数量
+func WithMaxBackups(maxBackups int) ConfigOption {
+	return func(loggerConfig *Config) error {
+		loggerConfig.MaxBackups = maxBackups
+		return nil
+	}
+}
+
+// WithMaxAge 设置旧日志文件保留天数
+func WithMaxAge(maxAge int) ConfigOption {
+	return func(loggerConfig *Config) error {
+		loggerConfig.MaxAge = maxAge
+		return nil
+	}
+}
+
+// WithCompress 设置是否压缩旧日志文件
+func WithCompress(compress bool) ConfigOption {
+	return func(loggerConfig *Config) error {
+		loggerConfig.Compress = compress
+		return nil
+	}
+}
+
+// WithLogLevel 设置日志等级
+func WithLogLevel(logLevel string) ConfigOption {
+	return func(loggerConfig *Config) error {
+		loggerConfig.LogLevel = logLevel
+		return nil
+	}
+}
+
+// WithLogFormat 设置日志格式
+func WithLogFormat(logFormat string) ConfigOption {
+	return func(loggerConfig *Config) error {
+		loggerConfig.LogFormat = logFormat
+		return nil
+	}
+}
+
+// NewDefaultConfig 创建默认配置
+func NewDefaultConfig() *Config {
+	return &Config{
+		LogFile:    "app.log",
+		MaxSize:    1,
+		MaxBackups: 5,
+		MaxAge:     30,
+		Compress:   false,
+		LogLevel:   "info",
+		LogFormat:  "json",
+	}
+}
+
 // InitLogger 初始化Logger
-func InitLogger(cfg *config.Config) (err error) {
+func InitLogger(logConfig Config, options ...ConfigOption) (err error) {
 	logLevel := map[string]zapcore.Level{
 		"debug": zapcore.DebugLevel,
 		"info":  zapcore.InfoLevel,
 		"warn":  zapcore.WarnLevel,
 		"error": zapcore.ErrorLevel,
 	}
-	logConfig := cfg.LogConfig
 	writeSyncer := getLogWriter(logConfig.LogFile, logConfig.MaxSize, logConfig.MaxBackups, logConfig.MaxAge, logConfig.Compress)
 
 	logFormats := []string{"json", "logfmt"}
