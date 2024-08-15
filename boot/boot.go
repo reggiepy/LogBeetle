e4deca2735a3f6daf33e5d3f03628f9ee56ad858
root <wangtongpy@foxmail.com>
2024-08-15 14:51:21 +0800

    fix run http server

--- boot/boot.go
+++ boot/boot.go
@@ -16,7 +16,7 @@ func Boot() {
 	}
 	go func() {
 		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			global.LbLogger.Info(fmt.Sprintf("start http server error: %v\n", err))
+			global.LbLogger.Fatal(fmt.Sprintf("start http server error: %v\n", err))
 		}
 	}()
 	logo(addr)
