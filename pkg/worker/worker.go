c2eb8e24d4be1447fe1e491d38785da17074a1e2
root <wangtongpy@foxmail.com>
2024-08-19 10:42:21 +0800

    refactor(worker): optimize Worker struct and Run method
    - Simplified the initialization of `Worker` struct by removing redundant code in `NewWorker` function.
    - Removed unnecessary `panic` calls; instead, errors are now returned and handled more gracefully.
    - Enhanced goroutine management in the `Run` method to ensure proper handling of `WaitGroup` and resource cleanup.
    - Replaced `fmt.Printf` with `log.Printf` for standardized logging.
    - Improved overall code readability and robustness.

--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -16,37 +16,39 @@ type Worker struct {
 }
 
 // NewWorker 创建一个新的 Worker 实例，接受一个或多个选项
-func NewWorker(options ...Option) *Worker {
-	var (
-		err error
-		w   = &Worker{}
-	)
+func NewWorker(options ...Option) (*Worker, error) {
+	w := &Worker{}
 	for _, opt := range options {
-		err = opt.apply(w)
-		if err != nil {
-			panic(err.(any))
+		if err := opt.apply(w); err != nil {
+			return nil, err
 		}
 	}
-	return w
+	return w, nil
 }
+
 
 // Run 启动工作器，执行具体的工作
 func (w *Worker) Run() {
+	if w.Wg != nil {
+		w.Wg.Add(1)
+	}
+
 	go func() {
-		defer w.Wg.Done()
-		for {
-			select {
-			case <-w.Ctx.Done():
-				fmt.Printf("【%s】Worker stopped\n", w.Name)
-				w.Stop()
-				return
-				// default:
-				//	// 在这里执行具体的工作
+		defer func() {
+			if w.Wg != nil {
+				w.Wg.Done()
 			}
-		}
-	}()
-	go func() {
+		}()
 		fmt.Printf("【%s】Working Start...\n", w.Name)
-		w.Start()
+		if w.Start != nil {
+			w.Start()
+		}
+
+		<-w.Ctx.Done()
+
+		fmt.Printf("【%s】Worker stopped\n", w.Name)
+		if w.Stop != nil {
+			w.Stop()
+		}
 	}()
 }
